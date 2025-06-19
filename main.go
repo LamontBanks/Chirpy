668b904bcfa850238c6a38a650130dcb29969005
Lamont Banks <lamontbanks@gmail.com>
2025-06-19 19:03:16 -0400

    `/healthz` check, index.html for logo

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
 	// Matches incoming URL requests to registered patterns and calls the attached handlers
 	mux := http.NewServeMux()
 
 	// Handlers
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthHandler)
 
+	// Set handlers, port
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 
 	server.ListenAndServe()
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
