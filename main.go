fb296d2a051ba2a39373cee4f57353a9d3ff1e88
Lamont Banks <lamontbanks@gmail.com>
2025-06-19 16:27:52 -0400

    Serve `index.html`

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	// Matches incoming URL requests to registered patterns and calls the attached handlers
 	mux := http.NewServeMux()
 
+	// Handlers
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
