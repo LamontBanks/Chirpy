fa5bb5cab716f79a4ab1ab8ba87b9608fe76c7af
Lamont Banks <lamontbanks@gmail.com>
2025-06-21 11:50:31 -0400

    Use middleware to count, view, and reset api hits

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+// Store stateful data between API calls
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
 func main() {
+	cfg := &apiConfig{}
+	cfg.fileServerHits.Store(0)
+
 	// Matches incoming URL requests to registered patterns and calls the attached handlers
 	mux := http.NewServeMux()
 
 	// Handlers
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", healthHandler)
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+	mux.HandleFunc("/reset", cfg.resetMetricsHandler)
 
 	// Set handlers, port
 	server := &http.Server{
@@ -21,8 +33,34 @@ func main() {
 	server.ListenAndServe()
 }
 
+// Handlers
+
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+// Increments the number of hits to the server for the given endpoint handler
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	fmt.Fprintf(w, "Hits: %v", cfg.fileServerHits.Load())
+}
+
+// Resets the hit count to 0
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
