c906192a82ebbc0e602d23b24bc9b372584e6b62
Lamont Banks <lamontbanks@gmail.com>
2025-07-01 10:21:03 -0400

    Basic implementation of a JWT (JSON Web Token) + few unit tests
    JWT contains the userID

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,9 +1,25 @@
 package auth
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Custom JWT Token with Claims
+// https://pkg.go.dev/github.com/golang-jwt/jwt/v5#NewWithClaims
+// jwt.RegisterdClaims can used on its own, but wrapping in a custom Claims is the typical use case
+// This allows additional public/private data to be added to the token
+// Currently not adding any add'l info, but will follow the recommended usage:
+// https://pkg.go.dev/github.com/golang-jwt/jwt/v5#example-NewWithClaims-CustomClaimsType
+type CustomClaims struct {
+	jwt.RegisteredClaims
+}
+
+// Returns `bcrypt`-hashed password
 func HashPassword(password string) (string, error) {
 	passwordBytes := []byte(password)
 	hashedBytes, err := bcrypt.GenerateFromPassword(passwordBytes, bcrypt.DefaultCost)
@@ -14,9 +30,54 @@ func HashPassword(password string) (string, error) {
 	return string(hashedBytes), nil
 }
 
+// Returns nil if plaintext password matches the hashed password, error otherwise
 func CheckPasswordHash(password, hash string) error {
 	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
 		return err
 	}
 	return nil
+}
+
+// Returns a JSON Web Token (JWT) for the given user
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	if userID == uuid.Nil || tokenSecret == "" {
+		return "", fmt.Errorf("invalid userID: %v, tokenSecret: %v", userID, tokenSecret)
+	}
+
+	// Create token
+	claims := CustomClaims{
+		jwt.RegisteredClaims{
+			Issuer:    "chirpy",
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+			Subject:   userID.String(),
+		},
+	}
+
+	// Create token, sign with given method
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+
+	return signedToken, err
+}
+
+func ValidateToken(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	claims, ok := token.Claims.(*CustomClaims)
+	if !ok {
+		return uuid.Nil, err
+	}
+
+	userId, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userId, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,13 @@
 package auth
 
-import "testing"
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
 
 func TestHashPassword(t *testing.T) {
 	// 1. Hash not the same as original password
@@ -39,6 +46,101 @@ func TestCheckPassword(t *testing.T) {
 	assertEqual(CheckPasswordHash(plaintextPassword, hashedPassword), nil, nil, t)
 }
 
+func TestJWTGeneratesAToken(t *testing.T) {
+	userID := uuid.New()
+	tokenSecret := "acb123xyz!@#"
+	expiresIn, _ := time.ParseDuration("1h")
+
+	token, err := MakeJWT(userID, tokenSecret, expiresIn)
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	assertNotEqual(token, "", nil, t)
+}
+
+func TestValidateTokenExtractsUserID(t *testing.T) {
+	// Create token, embedding userID
+	userID := uuid.New()
+	tokenSecret := "acb123xyz!@#"
+	expiresIn, _ := time.ParseDuration("1h")
+
+	token, err := MakeJWT(userID, tokenSecret, expiresIn)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// Extract userID from token
+	extractedUserID, err := ValidateToken(token, tokenSecret)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// Verify it's the same token
+	assertEqual(extractedUserID, userID, nil, t)
+}
+
+func TestValidateTokenRejectExpiredToken(t *testing.T) {
+	// Create new token with extremly short duration
+	userID := uuid.New()
+	tokenSecret := "acb123xyz!@#"
+	expiresIn, _ := time.ParseDuration("1ns")
+
+	token, err := MakeJWT(userID, tokenSecret, expiresIn)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// Allow token to expire
+	// TODO Replace sleep
+	expirationDuration, _ := time.ParseDuration("5ns")
+	time.Sleep(expirationDuration)
+
+	_, err = ValidateToken(token, tokenSecret)
+
+	// Check err message for invalid token and invalid claims error messages
+	// https://github.com/golang-jwt/jwt/blob/v5.2.2/errors.go#L8
+	expectedErrors := []error{
+		jwt.ErrTokenExpired,
+		jwt.ErrTokenInvalidClaims,
+	}
+
+	for i := range expectedErrors {
+		if !strings.Contains(err.Error(), expectedErrors[i].Error()) {
+			t.Errorf("missing expected expired token Error: '%v'", expectedErrors[i])
+		}
+	}
+}
+
+func TestValidateTokenRejectWrongTokenSecret(t *testing.T) {
+	// Create new token with
+	userID := uuid.New()
+	tokenSecret := "acb123xyz!@#"
+	expiresIn, _ := time.ParseDuration("1h")
+
+	token, err := MakeJWT(userID, tokenSecret, expiresIn)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// Attempt to read with wrong secret key
+	wrongTokenSecret := "wrongSecret123"
+
+	_, err = ValidateToken(token, wrongTokenSecret)
+
+	// Check for expected error message(s)
+	expectedErrors := []error{
+		jwt.ErrTokenSignatureInvalid,
+	}
+
+	for i := range expectedErrors {
+		if !strings.Contains(err.Error(), expectedErrors[i].Error()) {
+			t.Errorf("missing expected invalid tokenSecret error: '%v'", expectedErrors[i])
+		}
+	}
+}
+
 func assertEqual(first, second, input any, t *testing.T) {
 	if first != second {
 		t.Errorf("\nInput:\n\t%v\nActual:\n\t%v\nExpected:\n\t%v", input, first, second)
@@ -47,6 +149,6 @@ func assertEqual(first, second, input any, t *testing.T) {
 
 func assertNotEqual(first, second, input any, t *testing.T) {
 	if first == second {
-		t.Errorf("\nInput:\n\t%v\nActual:\n\t%v\nExpected:\n\t%v", input, first, second)
+		t.Errorf("\nInput:\n\t%v\nActual:\n\t%v\nExpected:\n\t*not* %v", input, first, second)
 	}
 }
