a9c2343ea044733e68bd36e4b15d7920f5c1a603
Lamont Banks <lamontbanks@gmail.com>
2025-07-08 00:23:10 -0400

    Remove duplicate test

--- validate_chirp_test.go
+++ validate_chirp_test.go
@@ -105,27 +105,6 @@ func TestValidateChirpHandler(t *testing.T) {
 	}
 }
 
-func TestTooLongChirp(t *testing.T) {
-	inputBody := `{"body": "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}`
-	expectedStatusCode := http.StatusBadRequest
-	expectedBody := `{"error":"Chirp is too long"}`
-
-	request := httptest.NewRequest("POST", "/api/validate_chirp", strings.NewReader(inputBody))
-	request.Header.Set("Content-Type", "application/json")
-
-	httpRecorder := httptest.NewRecorder()
-	validateChirpHandler(httpRecorder, request)
-
-	response := httpRecorder.Result()
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		t.Errorf("%v", err)
-	}
-
-	assertEquals(response.StatusCode, expectedStatusCode, nil, t)
-	assertEquals(string(body), expectedBody, inputBody, t)
-}
-
 // Check if actual == expected
 // `input` is an optional includsion of the original input
 func assertEquals(actual, expected, input any, t *testing.T) {
